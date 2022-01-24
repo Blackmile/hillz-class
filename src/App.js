b268b31a33814140b9634f26c643fc03fc209a67
Stanley Eze <stanley.ezeifeanyi@gmail.com>
2022-01-24 03:03:26 +0100

    make axios calls from api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
 import Main from './Main';
 import SideBar from './SideBar';
+import axios from './axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [classDetails, setClassDetails] = useState([])
+
+  useEffect(() => {
+    const fetchClassDetails = async() => await axios.get('/sync').then(response => {
+      setClassDetails(response.data)
+    });
+
+    fetchClassDetails();
+  }, [])
+
+  console.log(classDetails)
+
   return (
     <div className="app">
       <div className='app_body'>
         <SideBar />
-        <Main />
+        <Main classDetails = {classDetails} />
       </div>
     </div>
   );
